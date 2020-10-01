68eefa7221ffba8ad84b6bd0f08ef5731eebf21c
Camille Després <camille.lilalo@gmail.com>
2020-10-01 18:34:58 -0400

    Finished functions for non-numeric buttons

--- SimpleCalculator/app/src/main/java/com/lab3_103/simplecalculator/MainActivity.java
+++ SimpleCalculator/app/src/main/java/com/lab3_103/simplecalculator/MainActivity.java
@@ -9,7 +9,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private enum Operator {none, add, minus, muliply, divide};
+    private enum Operator {none, add, minus, multiply, divide};
     private double data1 =0, data2 =0;
     private Operator optr = Operator.none;
 
@@ -67,5 +67,64 @@ public class MainActivity extends AppCompatActivity {
         eText.setText(eText.getText()+"9");
     }
 
+    public void btnAddClick(View view){
+        optr = Operator.add;
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        data1 = Double.parseDouble(eText.getText().toString());
+        eText.setText("");
+    }
 
+    public void btnMinusClick(View view){
+        optr = Operator.minus;
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        data1 = Double.parseDouble(eText.getText().toString());
+        eText.setText("");
+    }
+
+    public void btnMultiplyClick(View view) {
+        optr = Operator.multiply;
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        data1 = Double.parseDouble(eText.getText().toString());
+        eText.setText("");
+    }
+
+    public void btnDivideClick(View view) {
+        optr = Operator.divide;
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        data1 = Double.parseDouble(eText.getText().toString());
+        eText.setText("");
+    }
+
+    public void btnClearClick(View view) {
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText("");
+    }
+
+    public void btnDotClick(View view) {
+        EditText eText = (EditText)findViewById(R.id.resultEdit);
+        eText.setText(eText.getText()+".");
+    }
+
+    public void btnResultClick(View view) {
+        if (optr != Operator.none) {
+            EditText eText = (EditText) findViewById(R.id.resultEdit);
+            data2 = Double.parseDouble(eText.getText().toString());
+            double result = 0;
+            if (optr == Operator.add) {
+                result = data1 + data2;
+            } else if (optr == Operator.minus) {
+                result = data1 - data2;
+            } else if (optr == Operator.multiply) {
+                result = data1 * data2;
+            } else if (optr == Operator.divide) {
+                result = data1 / data2;
+            }
+            optr = Operator.none;
+            data1 = result;
+            if ((result - (int) result) != 0)
+                eText.setText(String.valueOf(result));
+            else
+                eText.setText(String.valueOf((int) result));
+        }
+    }
 }
